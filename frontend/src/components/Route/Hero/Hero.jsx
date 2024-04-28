3b1e02e7e1a798c74af888406c4b737fef858e39
Ehtisham Rehmat <a7rehmat@gmail.com>
2024-04-28 14:04:15 +0500

    front end added with some errors, might fix them later

---
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
+
+const Hero = () => {
+  return (
+    <div
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[55%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+        >
+          Best Collection for <br /> home Decoration
+        </h1>
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
+          <p className="pb-2">Step into a realm of lifestyle luxury at <span className="italic font-[500]">Vendorverse!</span></p> 
+          Elevate every corner of your home and enrich your life with our exquisite collections. 
+          From chic decor pieces to elegant furnishings, our curated selection will inspire you to transform
+          your living space into a sanctuary of style and comfort. Explore our best-in-class offerings and add a 
+          touch of sophistication to your lifestyle.<br/>
+          <p className="pt-2">
+          Welcome to Vendorverse, where your dream home and lifestyle come to life.
+          </p>
+        </p>
+        <Link to="/products" className="inline-block">
+            <div className={`${styles.button} mt-5`}>
+                 <span className="text-[#fff] font-[Poppins] text-[18px]">
+                    Shop Now
+                 </span>
+            </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
