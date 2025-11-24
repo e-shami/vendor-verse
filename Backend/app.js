5d33beaa60c16c31accbd88471a0f5d141925655
Ehtisham Rehmat <a7rehmat@gmail.com>
2025-11-24 16:09:29 +0500

    testing all the changes for backend to be deployed on vercel

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000" || "https://vendor-verse-phi.vercel.app/"],
     credentials: true,
   })
 );
